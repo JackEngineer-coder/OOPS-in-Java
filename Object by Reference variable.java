62a00ab999a7fc41b5354f065a5a423ea20a0000
Jitender Chauhan <66746889+JackEngineer-coder@users.noreply.github.com>
2022-03-16 00:01:53 +0530

    Create Object by Reference variable.java

---
+++ Object by Reference variable.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Animal{
+    String name;
+    int age;
+  public static void main(String[] args)
+  {
+    Animal Dog = new Animal();
+    Dog.name = "buzo";
+    Dog.age = 10;
+
+    System.out.println(Dog.name + " "+ Dog.age);
+    
+  }
+}
