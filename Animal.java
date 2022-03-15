f32420d99fa9a9e337211d8d87c226b5cfdc5654
Jitender Chauhan <66746889+JackEngineer-coder@users.noreply.github.com>
2022-03-15 23:07:49 +0530

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,17 @@
+
+class Animal{
+  public static void main(String[] args)
+  {
+    Animal buzo = new Animal();
+    buzo.Run();
+    buzo.Eat();
+    
+  }
+    public void Run(){
+      System.out.println("I am Running");
+    }
+
+    public void Eat(){
+      System.out.println("I am Eating");
+    }
+}
