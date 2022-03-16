edd4194de2854b8a40bfb5666420691f20b56b18
Jitender Chauhan <66746889+JackEngineer-coder@users.noreply.github.com>
2022-03-16 23:42:13 +0530

    Create Default_Construcot.java
    Default constructor created by the compiler, not by JVM
    Default constructor created by JVM when not even single constructor created by programer in the whole program.
    Default constructor returns zero in integers and null in string.
    It is no -argument constructor type

---
+++ Default_Construcot.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Test{
+    String name;
+    int marks;
+  public static void main(String[] args)
+  {
+    Test t = new Test();
+    System.out.println(t.name);
+    System.out.println(t.marks);
+  }
+}
