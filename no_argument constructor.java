206a0e8e449d908bd032cf23f2e75b03bed0968c
Jitender Chauhan <66746889+JackEngineer-coder@users.noreply.github.com>
2022-03-16 23:48:57 +0530

    Create no_argument constructor.java
    It is created by programer.

---
+++ no_argument constructor.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Test{
+  public Test(){
+    System.out.println("no argument construtor");
+  }
+  public static void main(String[] args)
+    {
+      Test t = new Test();
+    }
+}
