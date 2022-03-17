471f68b67312855d3bf20a8392cc16f77af0b853
Jitender Chauhan <66746889+JackEngineer-coder@users.noreply.github.com>
2022-03-17 21:34:31 +0530

    Create Single_level_Inheritance.java

---
+++ Single_level_Inheritance.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Animal{
+  public void eat(){
+    System.out.println("I am eating");
+  }
+}
+public class Dog extends Animal{
+  public static void main(String[] args)
+  {
+  Dog d = new Dog();
+  d.eat();
+}
+}
