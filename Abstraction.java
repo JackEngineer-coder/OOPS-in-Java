b8e2a441f77cf96d4d7194911b94f5b1c64ebf45
Jitender Chauhan <66746889+JackEngineer-coder@users.noreply.github.com>
2022-03-27 23:54:28 +0530

    Create Abstraction.java
    1. A method without a body is known as the Abstract method.
    2. if a method created as abstract method then the class in which we have created it should declared as abstract class.
    3. if a regular class extends an abstract class , then all the abstract method of abstract parent class should be implemented in the child class.
    4. Abstract method in abstract class meant to be overridden in derived concrete classes otherwise compile time error will be thrown.
    5.Abstract class can't instantiated means we can't create object of abstract class.
    6. Inheritance, polymorphism are used to increase code reusability but Abstraction used for security purpose.
    7. In abstraction we show the desired part only and we hides all the unnecessary part or part to be hidden.

---
+++ Abstraction.java
@@ -0,0 +1,23 @@
+import java.util.*;
+abstract class vehicle{
+  abstract void start();
+  
+}
+class car extends vehicle{
+  void start(){
+    System.out.println("Starts by Key");
+  }
+}
+public class scooter extends vehicle{
+  void start(){
+    System.out.println("Starts by kick");
+  }
+  public static void main(String[] args)
+  {
+    car alto = new car();
+    alto.start();
+
+    scooter bajaj = new scooter();
+    bajaj.start();
+  }
+}
